a4cedc107a29a9e248c6ce38f4d6c5ab18000958
beyond88 <muhin.cse.diu@gmail.com>
2024-10-24 01:20:28 +0600

    add comment

--- includes/blocks.php
+++ includes/blocks.php
@@ -1,18 +1,26 @@
 <?php
 namespace BentoNinja;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit(); // Exit if accessed directly.
-}
+defined('ABSPATH') || exit;
 
 class Blocks {
 
+    /**
+     * Initialize the Bento Ninja Plugin.
+     *
+     * @return void
+     */
     public static function init() {
 		$self = new self();
 		// block initialization
 		add_action( 'init', [ $self, 'blocks_init' ] );
 	}
 
+    /**
+     * Register and initialize Gutenberg blocks.
+     *
+     * @return void
+     */
     public function blocks_init() {
 
     }
